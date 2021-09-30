d678b3f1cbb36b870539ae2b6dd603bd2fafccfb
zhang-wei <zhangwei24@apache.org>
2021-09-30 23:13:50 +0800

    Update dubbo-go/test/server/cmd/server.go

--- dubbo-go/test/server/cmd/server.go
+++ dubbo-go/test/server/cmd/server.go
@@ -69,9 +69,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("set tracer error: %v \n", err)
 	}
-	dubbo_go.SetClientExtraTags("extra-tags", "server")
-	dubbo_go.SetClientReportTags("release")
-	dubbo_go.SetClientReportTags("organization")
+	dubbo_go.SetServerExtraTags("extra-tags", "server")
+	dubbo_go.SetServerReportTags("release")
+	dubbo_go.SetServerReportTags("organization")
 
 	initSignal()
 }
