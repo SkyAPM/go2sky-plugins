6e7bb1d328a49811b5f5eb5ecda17da5e4777bdb
zhangwei <zhangwei24@apache.org>
2021-10-09 16:17:51 +0800

    test: replace range with contains

--- resty/test/client/client.go
+++ resty/test/client/client.go
@@ -58,10 +58,6 @@ func main() {
 		_, _ = writer.Write(resp.Body())
 	})
 
-	route.HandleFunc("/healthCheck", func(writer http.ResponseWriter, request *http.Request) {
-		_, _ = writer.Write([]byte("Success"))
-	})
-
 	sm, err := httpPlugin.NewServerMiddleware(tracer)
 	if err != nil {
 		log.Fatalf("create client error %v \n", err)
