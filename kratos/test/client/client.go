d3ff1f638870a6883334df7698cf152e712bc413
Kagaya <kagaya85@outlook.com>
2022-04-07 12:44:21 +0800

    update kratos to v2.2.1

--- kratos/test/client/client.go
+++ kratos/test/client/client.go
@@ -24,7 +24,7 @@ import (
 	"github.com/SkyAPM/go2sky"
 	kratosplugin "github.com/SkyAPM/go2sky-plugins/kratos"
 	"github.com/SkyAPM/go2sky/reporter"
-	"github.com/go-kratos/kratos/examples/helloworld/helloworld"
+	"github.com/go-kratos/examples/helloworld/helloworld"
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
